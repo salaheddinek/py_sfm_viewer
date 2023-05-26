b9aa5d23291f5e80a94ac8bcf8ecdb3a7a762858
salaheddinek <salaheddinek@protonmail.ch>
2023-05-26 21:25:57 +0200

    Added gui icons

--- src/ui_core.py
+++ src/ui_core.py
@@ -8,19 +8,23 @@ import ui_data
 
 class ViewerGui(Qw.QMainWindow):
     def __init__(self, in_args, in_params):
-        print("gui init")
+        print("SFM Viewer: Gui mode")
         super(ViewerGui, self).__init__()
+        self.i_app = ui_data.qt_icon_from_text_image(ui_data.APP_ICON)
+        self.setWindowIcon(self.i_app)
 
         # self.i = qt_icons.qt_icon_from_text_image(qt_icons.APP_ICON)
         # self.setWindowIcon(self.i)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # self.setFont(Qg.QFont())
         self.setWindowTitle("SFM Viewer")
         self._style_app()
-        # self._connect_signals()
+        self._connect_signals()
         # self.update_ui_elements()
+
+    def _connect_signals(self):
+        self.ui.btn_exit.clicked.connect(self.close_app)
 
     def _style_app(self):
         f_id = Qg.QFontDatabase.addApplicationFontFromData(ui_data.text_data_to_bytes(ui_data.FONT_DATA_OPEN_SANS))
@@ -33,10 +37,34 @@ class ViewerGui(Qw.QMainWindow):
                     "QTextEdit", "QPlainTextEdit"]:
             Qw.QApplication.setFont(Qg.QFont(families[0], 12), wid)
         self.setStyleSheet(ui_style.APP_STYLESHEET)
-        self._set_css_class(self.ui.pushButton, "action")
-        self._set_css_class(self.ui.pushButton_2, "danger")
+        self._set_css_class(self.ui.btn_ok, "action")
+        self._set_css_class(self.ui.btn_exit, "danger")
+
+        self.i_info = ui_data.qt_icon_from_text_image(ui_data.INFO_ICON)
+        for wid in [self.ui.btn_info_view_mode, self.ui.btn_info_cone_size, self.ui.btn_info_subsample_mode, self.ui.btn_info_subsample_factor]:
+            wid.setIcon(self.i_info)
+
+        self.i_save = ui_data.qt_icon_from_text_image(ui_data.SAVE_ICON)
+        self.ui.btn_ok.setIcon(self.i_save)
+
+        self.i_exit = ui_data.qt_icon_from_text_image(ui_data.EXIT_ICON)
+        self.ui.btn_exit.setIcon(self.i_exit)
+
+        self.i_path = ui_data.qt_icon_from_text_image(ui_data.PATH_ICON)
+        self.ui.btn_in_path.setIcon(self.i_path)
+        self.ui.btn_out_path.setIcon(self.i_path)
+
+        self.i_back = ui_data.qt_icon_from_text_image(ui_data.BACK_ICON)
+        self.ui.btn_go_back.setIcon(self.i_back)
+
+        self.i_colors = ui_data.qt_icon_from_text_image(ui_data.COLORS_ICON)
+        self.ui.btn_first_color.setIcon(self.i_colors)
+        self.ui.btn_last_color.setIcon(self.i_colors)
 
     @staticmethod
     def _set_css_class(widget, class_name):
         widget.setProperty("cssClass", class_name)
         widget.style().polish(widget)
+
+    def close_app(self):
+        self.close()
--- src/ui_mainwindow.py
+++ src/ui_mainwindow.py
@@ -36,30 +36,30 @@ class Ui_MainWindow(object):
 
         self.gridLayout_3.addWidget(self.label_5, 0, 0, 1, 1)
 
-        self.pushButton_5 = QPushButton(self.centralwidget)
-        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.btn_out_path = QPushButton(self.centralwidget)
+        self.btn_out_path.setObjectName(u"btn_out_path")
 
-        self.gridLayout_3.addWidget(self.pushButton_5, 1, 2, 1, 1)
+        self.gridLayout_3.addWidget(self.btn_out_path, 1, 2, 1, 1)
 
-        self.lineEdit = QLineEdit(self.centralwidget)
-        self.lineEdit.setObjectName(u"lineEdit")
+        self.line_edit_in_path = QLineEdit(self.centralwidget)
+        self.line_edit_in_path.setObjectName(u"line_edit_in_path")
 
-        self.gridLayout_3.addWidget(self.lineEdit, 0, 1, 1, 1)
+        self.gridLayout_3.addWidget(self.line_edit_in_path, 0, 1, 1, 1)
 
-        self.pushButton_4 = QPushButton(self.centralwidget)
-        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.btn_in_path = QPushButton(self.centralwidget)
+        self.btn_in_path.setObjectName(u"btn_in_path")
 
-        self.gridLayout_3.addWidget(self.pushButton_4, 0, 2, 1, 1)
+        self.gridLayout_3.addWidget(self.btn_in_path, 0, 2, 1, 1)
 
         self.label_6 = QLabel(self.centralwidget)
         self.label_6.setObjectName(u"label_6")
 
         self.gridLayout_3.addWidget(self.label_6, 1, 0, 1, 1)
 
-        self.lineEdit_2 = QLineEdit(self.centralwidget)
-        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.line_edit_out_path = QLineEdit(self.centralwidget)
+        self.line_edit_out_path.setObjectName(u"line_edit_out_path")
 
-        self.gridLayout_3.addWidget(self.lineEdit_2, 1, 1, 1, 1)
+        self.gridLayout_3.addWidget(self.line_edit_out_path, 1, 1, 1, 1)
 
 
         self.verticalLayout_2.addLayout(self.gridLayout_3)
@@ -75,35 +75,38 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
 
-        self.doubleSpinBox = QDoubleSpinBox(self.groupBox)
-        self.doubleSpinBox.setObjectName(u"doubleSpinBox")
+        self.dspin_box_cone_size = QDoubleSpinBox(self.groupBox)
+        self.dspin_box_cone_size.setObjectName(u"dspin_box_cone_size")
+        self.dspin_box_cone_size.setDecimals(4)
+        self.dspin_box_cone_size.setMinimum(0.000100000000000)
+        self.dspin_box_cone_size.setSingleStep(0.010000000000000)
 
-        self.gridLayout.addWidget(self.doubleSpinBox, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.dspin_box_cone_size, 1, 1, 1, 1)
 
         self.label_2 = QLabel(self.groupBox)
         self.label_2.setObjectName(u"label_2")
 
         self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
 
-        self.comboBox = QComboBox(self.groupBox)
-        self.comboBox.setObjectName(u"comboBox")
+        self.combo_box_view_mode = QComboBox(self.groupBox)
+        self.combo_box_view_mode.setObjectName(u"combo_box_view_mode")
         sizePolicy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
-        self.comboBox.setSizePolicy(sizePolicy)
+        sizePolicy.setHeightForWidth(self.combo_box_view_mode.sizePolicy().hasHeightForWidth())
+        self.combo_box_view_mode.setSizePolicy(sizePolicy)
 
-        self.gridLayout.addWidget(self.comboBox, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.combo_box_view_mode, 0, 1, 1, 1)
 
-        self.toolButton = QToolButton(self.groupBox)
-        self.toolButton.setObjectName(u"toolButton")
+        self.btn_info_view_mode = QToolButton(self.groupBox)
+        self.btn_info_view_mode.setObjectName(u"btn_info_view_mode")
 
-        self.gridLayout.addWidget(self.toolButton, 0, 2, 1, 1)
+        self.gridLayout.addWidget(self.btn_info_view_mode, 0, 2, 1, 1)
 
-        self.toolButton_2 = QToolButton(self.groupBox)
-        self.toolButton_2.setObjectName(u"toolButton_2")
+        self.btn_info_cone_size = QToolButton(self.groupBox)
+        self.btn_info_cone_size.setObjectName(u"btn_info_cone_size")
 
-        self.gridLayout.addWidget(self.toolButton_2, 1, 2, 1, 1)
+        self.gridLayout.addWidget(self.btn_info_cone_size, 1, 2, 1, 1)
 
 
         self.horizontalLayout.addWidget(self.groupBox)
@@ -112,32 +115,35 @@ class Ui_MainWindow(object):
         self.groupBox_2.setObjectName(u"groupBox_2")
         self.gridLayout_4 = QGridLayout(self.groupBox_2)
         self.gridLayout_4.setObjectName(u"gridLayout_4")
-        self.doubleSpinBox_2 = QDoubleSpinBox(self.groupBox_2)
-        self.doubleSpinBox_2.setObjectName(u"doubleSpinBox_2")
+        self.dspin_box_subsample_factor = QDoubleSpinBox(self.groupBox_2)
+        self.dspin_box_subsample_factor.setObjectName(u"dspin_box_subsample_factor")
+        self.dspin_box_subsample_factor.setDecimals(4)
+        self.dspin_box_subsample_factor.setMinimum(0.000100000000000)
+        self.dspin_box_subsample_factor.setSingleStep(0.010000000000000)
 
-        self.gridLayout_4.addWidget(self.doubleSpinBox_2, 2, 3, 1, 1)
+        self.gridLayout_4.addWidget(self.dspin_box_subsample_factor, 2, 3, 1, 1)
 
-        self.toolButton_3 = QToolButton(self.groupBox_2)
-        self.toolButton_3.setObjectName(u"toolButton_3")
+        self.btn_info_subsample_mode = QToolButton(self.groupBox_2)
+        self.btn_info_subsample_mode.setObjectName(u"btn_info_subsample_mode")
 
-        self.gridLayout_4.addWidget(self.toolButton_3, 1, 4, 1, 1)
+        self.gridLayout_4.addWidget(self.btn_info_subsample_mode, 1, 4, 1, 1)
 
         self.label_4 = QLabel(self.groupBox_2)
         self.label_4.setObjectName(u"label_4")
 
         self.gridLayout_4.addWidget(self.label_4, 2, 0, 1, 1)
 
-        self.comboBox_2 = QComboBox(self.groupBox_2)
-        self.comboBox_2.setObjectName(u"comboBox_2")
-        sizePolicy.setHeightForWidth(self.comboBox_2.sizePolicy().hasHeightForWidth())
-        self.comboBox_2.setSizePolicy(sizePolicy)
+        self.combo_box_subsample_mode = QComboBox(self.groupBox_2)
+        self.combo_box_subsample_mode.setObjectName(u"combo_box_subsample_mode")
+        sizePolicy.setHeightForWidth(self.combo_box_subsample_mode.sizePolicy().hasHeightForWidth())
+        self.combo_box_subsample_mode.setSizePolicy(sizePolicy)
 
-        self.gridLayout_4.addWidget(self.comboBox_2, 1, 3, 1, 1)
+        self.gridLayout_4.addWidget(self.combo_box_subsample_mode, 1, 3, 1, 1)
 
-        self.toolButton_4 = QToolButton(self.groupBox_2)
-        self.toolButton_4.setObjectName(u"toolButton_4")
+        self.btn_info_subsample_factor = QToolButton(self.groupBox_2)
+        self.btn_info_subsample_factor.setObjectName(u"btn_info_subsample_factor")
 
-        self.gridLayout_4.addWidget(self.toolButton_4, 2, 4, 1, 1)
+        self.gridLayout_4.addWidget(self.btn_info_subsample_factor, 2, 4, 1, 1)
 
         self.label_3 = QLabel(self.groupBox_2)
         self.label_3.setObjectName(u"label_3")
@@ -154,17 +160,17 @@ class Ui_MainWindow(object):
         self.groupBox_3.setObjectName(u"groupBox_3")
         self.verticalLayout = QVBoxLayout(self.groupBox_3)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.comboBox_3 = QComboBox(self.groupBox_3)
-        self.comboBox_3.setObjectName(u"comboBox_3")
+        self.combo_box_colormap = QComboBox(self.groupBox_3)
+        self.combo_box_colormap.setObjectName(u"combo_box_colormap")
 
-        self.verticalLayout.addWidget(self.comboBox_3)
+        self.verticalLayout.addWidget(self.combo_box_colormap)
 
-        self.gridLayout_2 = QGridLayout()
-        self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.pushButton_6 = QPushButton(self.groupBox_3)
-        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.wid_custom_colors = QGridLayout()
+        self.wid_custom_colors.setObjectName(u"wid_custom_colors")
+        self.btn_first_color = QPushButton(self.groupBox_3)
+        self.btn_first_color.setObjectName(u"btn_first_color")
 
-        self.gridLayout_2.addWidget(self.pushButton_6, 1, 0, 1, 1)
+        self.wid_custom_colors.addWidget(self.btn_first_color, 1, 0, 1, 1)
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
@@ -173,18 +179,18 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3.addWidget(self.label_7)
 
-        self.lineEdit_3 = QLineEdit(self.groupBox_3)
-        self.lineEdit_3.setObjectName(u"lineEdit_3")
+        self.line_edit_first_color = QLineEdit(self.groupBox_3)
+        self.line_edit_first_color.setObjectName(u"line_edit_first_color")
 
-        self.horizontalLayout_3.addWidget(self.lineEdit_3)
+        self.horizontalLayout_3.addWidget(self.line_edit_first_color)
 
 
-        self.gridLayout_2.addLayout(self.horizontalLayout_3, 0, 0, 1, 1)
+        self.wid_custom_colors.addLayout(self.horizontalLayout_3, 0, 0, 1, 1)
 
-        self.pushButton_7 = QPushButton(self.groupBox_3)
-        self.pushButton_7.setObjectName(u"pushButton_7")
+        self.btn_last_color = QPushButton(self.groupBox_3)
+        self.btn_last_color.setObjectName(u"btn_last_color")
 
-        self.gridLayout_2.addWidget(self.pushButton_7, 1, 1, 1, 1)
+        self.wid_custom_colors.addWidget(self.btn_last_color, 1, 1, 1, 1)
 
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
@@ -193,21 +199,21 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_4.addWidget(self.label_8)
 
-        self.lineEdit_4 = QLineEdit(self.groupBox_3)
-        self.lineEdit_4.setObjectName(u"lineEdit_4")
+        self.line_edit_last_color = QLineEdit(self.groupBox_3)
+        self.line_edit_last_color.setObjectName(u"line_edit_last_color")
 
-        self.horizontalLayout_4.addWidget(self.lineEdit_4)
-
-
-        self.gridLayout_2.addLayout(self.horizontalLayout_4, 0, 1, 1, 1)
+        self.horizontalLayout_4.addWidget(self.line_edit_last_color)
 
 
-        self.verticalLayout.addLayout(self.gridLayout_2)
+        self.wid_custom_colors.addLayout(self.horizontalLayout_4, 0, 1, 1, 1)
 
-        self.lineEdit_5 = QLineEdit(self.groupBox_3)
-        self.lineEdit_5.setObjectName(u"lineEdit_5")
 
-        self.verticalLayout.addWidget(self.lineEdit_5)
+        self.verticalLayout.addLayout(self.wid_custom_colors)
+
+        self.line_edit_view_color = QLineEdit(self.groupBox_3)
+        self.line_edit_view_color.setObjectName(u"line_edit_view_color")
+
+        self.verticalLayout.addWidget(self.line_edit_view_color)
 
 
         self.verticalLayout_2.addWidget(self.groupBox_3)
@@ -222,20 +228,20 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_2.addItem(self.horizontalSpacer_4)
 
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
+        self.btn_ok = QPushButton(self.centralwidget)
+        self.btn_ok.setObjectName(u"btn_ok")
 
-        self.horizontalLayout_2.addWidget(self.pushButton)
+        self.horizontalLayout_2.addWidget(self.btn_ok)
 
-        self.pushButton_3 = QPushButton(self.centralwidget)
-        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.btn_go_back = QPushButton(self.centralwidget)
+        self.btn_go_back.setObjectName(u"btn_go_back")
 
-        self.horizontalLayout_2.addWidget(self.pushButton_3)
+        self.horizontalLayout_2.addWidget(self.btn_go_back)
 
-        self.pushButton_2 = QPushButton(self.centralwidget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.btn_exit = QPushButton(self.centralwidget)
+        self.btn_exit.setObjectName(u"btn_exit")
 
-        self.horizontalLayout_2.addWidget(self.pushButton_2)
+        self.horizontalLayout_2.addWidget(self.btn_exit)
 
 
         self.verticalLayout_2.addLayout(self.horizontalLayout_2)
@@ -249,27 +255,27 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Inpput file", None))
-        self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"Path", None))
-        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"Path", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Input file", None))
+        self.btn_out_path.setText(QCoreApplication.translate("MainWindow", u"Path", None))
+        self.btn_in_path.setText(QCoreApplication.translate("MainWindow", u"Path", None))
         self.label_6.setText(QCoreApplication.translate("MainWindow", u"Output file", None))
-        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"GroupBox", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.toolButton.setText(QCoreApplication.translate("MainWindow", u"...", None))
-        self.toolButton_2.setText(QCoreApplication.translate("MainWindow", u"...", None))
-        self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"GroupBox", None))
-        self.toolButton_3.setText(QCoreApplication.translate("MainWindow", u"...", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.toolButton_4.setText(QCoreApplication.translate("MainWindow", u"...", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.groupBox_3.setTitle(QCoreApplication.translate("MainWindow", u"GroupBox", None))
-        self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"Change color", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Camera cones and geometry", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"View mode", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Cone size", None))
+        self.btn_info_view_mode.setText(QCoreApplication.translate("MainWindow", u"...", None))
+        self.btn_info_cone_size.setText(QCoreApplication.translate("MainWindow", u"...", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"Camera cones subsampling", None))
+        self.btn_info_subsample_mode.setText(QCoreApplication.translate("MainWindow", u"...", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Subsample factor", None))
+        self.btn_info_subsample_factor.setText(QCoreApplication.translate("MainWindow", u"...", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Subsample mode", None))
+        self.groupBox_3.setTitle(QCoreApplication.translate("MainWindow", u"Camera cone colors", None))
+        self.btn_first_color.setText(QCoreApplication.translate("MainWindow", u"Change color", None))
         self.label_7.setText(QCoreApplication.translate("MainWindow", u"First color", None))
-        self.pushButton_7.setText(QCoreApplication.translate("MainWindow", u"Change color", None))
+        self.btn_last_color.setText(QCoreApplication.translate("MainWindow", u"Change color", None))
         self.label_8.setText(QCoreApplication.translate("MainWindow", u"Last color", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"OK", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Go back", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Exit", None))
+        self.btn_ok.setText(QCoreApplication.translate("MainWindow", u"OK", None))
+        self.btn_go_back.setText(QCoreApplication.translate("MainWindow", u"Go back", None))
+        self.btn_exit.setText(QCoreApplication.translate("MainWindow", u"Exit", None))
     # retranslateUi
 
