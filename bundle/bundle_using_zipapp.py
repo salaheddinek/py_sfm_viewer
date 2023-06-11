ca6d03376b264ab8f1762c21fb864811d97ea51f
salaheddinek <salaheddinek@protonmail.ch>
2023-06-11 15:31:44 +0200

    added suffix argument to bundle script

--- bundle/bundle_using_zipapp.py
+++ bundle/bundle_using_zipapp.py
@@ -4,14 +4,14 @@ import sys
 import shutil
 import re
 import pathlib
+import argparse
 
 APP_NAME = "py_sfm_viewer"
-ADD_VERSION_AS_SUFFIX = True
 
 
-def get_app_full_name(i_app_path):
+def get_app_full_name(i_app_path, in_add_version_as_suffix):
     name = APP_NAME
-    if ADD_VERSION_AS_SUFFIX:
+    if in_add_version_as_suffix:
         with i_app_path .open("r") as file_obj:
             txt = file_obj.read()
             v_re = r"^__version__ = ['\"]([^'\"]*)['\"]"
@@ -27,19 +27,35 @@ def is_file_accepted(i_file_path):
     return False
 
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'on', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'off', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected, possible values: yes, y, true, 1, no, n, false, 0.')
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description='bundle the project files into one python file (pyz file) which '
+                                                 'can be copied  and used from anywhere in the system')
+    parser.add_argument('-s', '--add_suffix', help='add the version number as a suffix to the bundled file',
+                        type=str2bool, metavar='\b', default=False)
+    args = parser.parse_args()
     main_path = pathlib.Path(sys.modules['__main__'].__file__).parents[1]
     deploy_path = main_path / 'deploy'
-    app_path = main_path / 'src'
+    src_path = main_path / 'src'
 
-    app_name = get_app_full_name(app_path / "_version.py")
-
+    app_name = get_app_full_name(src_path / "_version.py", args.add_suffix) + ".pyz"
 
     if deploy_path.is_dir():
         shutil.rmtree(str(deploy_path))
     deploy_path.mkdir(parents=True)
 
-    zipapp.create_archive(app_path, str(deploy_path / (app_name + ".pyz")), '/usr/bin/python3',
+    zipapp.create_archive(src_path, str(deploy_path / app_name), '/usr/bin/python3',
                           filter=is_file_accepted)
 
-    print(f"wrapping finished! Output file: {app_name}.pyz")
+    print(f"wrapping finished! Output file: {app_name}")
